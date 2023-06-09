6eed2b683f84c6390780d718aef4ac28b7ae2b62
Christo du Toit <christo@dutoit.co.uk>
2023-06-09 10:46:07 +0100

    INFRASTRUCTURE: Initial checkin

---
+++ GithubActionExpressionTesting.Infrastructure.Build/Program.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+namespace GithubActionExpressionTesting.Infrastructure.Build
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ GithubActionExpressionTesting.Infrastructure.Tests/InfrastructureTests.cs
@@ -0,0 +1,19 @@
+// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Xunit;
+
+namespace GithubActionExpressionTesting.Infrastructure.Tests
+{
+    public class InfrastructureTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
